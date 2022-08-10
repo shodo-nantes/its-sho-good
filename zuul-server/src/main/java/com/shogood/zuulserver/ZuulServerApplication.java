c1067a67a57e1f732c6af7062efa5541be947a2d
Andgel Duport <andgel.duport@gmail.com>
2022-08-10 13:58:59 +0200

    Externalisation des configs

--- zuul-server/src/main/java/com/shogood/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/shogood/zuulserver/ZuulServerApplication.java
@@ -2,8 +2,10 @@ package com.shogood.zuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
+@EnableZuulServer
 public class ZuulServerApplication {
 
 	public static void main(String[] args) {
